89e042d42533521693dcb1309efdbf7ada467d66
Bakhodir <solievbakhodir.05@gmail.com>
2024-11-13 15:15:34 +0100

    created models for vendor, customer, and store

--- ecom_prj/urls.py
+++ ecom_prj/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 from store.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('', include('store.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
