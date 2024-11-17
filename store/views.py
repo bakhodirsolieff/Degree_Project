ca33568f404aa4268dc1f6aef31f74cb12598d00
Bakhodir <solievbakhodir.05@gmail.com>
2024-11-17 20:03:37 +0100

    worked on products view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store import models as store_models
+
 # Create your views here.
 def index(request):
-    return render(request, "store/index.html")
+    products = store_models.Product.objects.filter(status="Published")
+    for p in products:
+        pass
+    return render(request, "store/index.html", {"products":products})
